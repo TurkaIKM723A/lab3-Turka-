749513f85d9d6ad6e3ddf4dd4e146331d4cb2b7d
Jambi <iturkas42@gmail.com>
2025-04-05 19:04:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter size of ur array: ");
+        int size = int.Parse(Console.ReadLine());
+
+        int[] array = new int[size];
+
+        Console.WriteLine("Enter digits for ur array:");
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"Element [{i}]: ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        bool isPalindrome = true;
+
+        for (int i = 0; i < size / 2; i++)
+        {
+            if (array[i] != array[size - 1 - i])
+            {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome)
+        {
+            Console.WriteLine("Array is a palindrome.");
+        }
+        else
+        {
+            Console.WriteLine("Array is not a palindrome.");
+        }
+    }
+}
